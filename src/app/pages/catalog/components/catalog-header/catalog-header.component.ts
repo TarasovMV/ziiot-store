f9d5b56a2c5a33bf3156bee4461d3ff35ae23785
Mikhail Gunenkov <m.gunenkov@gmail.com>
2022-05-11 20:53:13 +0600

    Added arrows for scrolling

--- src/app/pages/catalog/components/catalog-header/catalog-header.component.ts
+++ src/app/pages/catalog/components/catalog-header/catalog-header.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
 import {ProductType} from '../../../../core/enums';
 import {inOutAnimation} from '../../../../shared/animations/in-out.animation';
+import {PlatformService} from "../../../../core/services/platform.service";
 
 @Component({
     selector: 'app-catalog-header',
@@ -12,4 +13,24 @@ import {inOutAnimation} from '../../../../shared/animations/in-out.animation';
 export class CatalogHeaderComponent {
     @Output() connect = new EventEmitter<string>();
     readonly productType = ProductType;
+    readonly isMobile$ = this.platformService.isMobile$;
+    private pageWrapper: HTMLElement | null;
+    private content?: Element;
+    private contentY?: number;
+
+    constructor(private readonly platformService: PlatformService) {
+        this.pageWrapper = document.getElementById("page-wrapper");
+        setTimeout(() => {
+            this.content = document.getElementsByClassName("content")[0];
+            const bodyRect = document.body.getBoundingClientRect();
+            const contentRect = this.content.getBoundingClientRect();
+            const offset   = contentRect.bottom - bodyRect.top;
+            this.contentY = offset;
+            console.log(offset);
+        }, 350);
+    }
+
+    scrollToCatalog() {
+        this.pageWrapper?.scrollTo({top: this.contentY, behavior: "smooth"});
+    }
 }
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
@@ -8,7 +8,22 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 })
 export class FooterComponent {
 
+    @ViewChild("arrowWrapper") arrowWrapper : ElementRef | undefined;
+    private pageWrapper: HTMLElement | null;
     constructor() {
+        this.pageWrapper = document.getElementById("page-wrapper");
+        this.pageWrapper?.addEventListener("scroll", () => {
+            if (!this.arrowWrapper) return;
+            // @ts-ignore
+            if (this.pageWrapper?.scrollTop > 100) {
+                this.arrowWrapper.nativeElement.style.display = "block";
+            } else {
+                this.arrowWrapper.nativeElement.style.display = "none";
+            }
+        });
     }
 
+    scrollTop() {
+        this.pageWrapper?.scrollTo({top: 0, behavior: "smooth"});
+    }
 }
