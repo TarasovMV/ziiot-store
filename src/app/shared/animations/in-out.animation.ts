f132d8c29ff7f31a8a58727fc0160dda93b787b5
Mikhail Gunenkov <m.gunenkov@gmail.com>
2022-06-25 15:46:26 +0600

    Translated form and reimplemented mobile menu in-out animations

--- src/app/shared/animations/in-out.animation.ts
+++ src/app/shared/animations/in-out.animation.ts
@@ -6,17 +6,17 @@ export const inOutAnimation = trigger(
         transition(
             ':enter',
             [
-                style({ height: 0, opacity: 1 }),
+                style({ right: '-100vw', opacity: 0 }),
                 animate('.3s ease-out',
-                    style({ height: '*', opacity: 1 }))
+                    style({ right: '0', opacity: 1 }))
             ]
         ),
         transition(
             ':leave',
             [
-                style({ height: '*', opacity: 1 }),
+                style({ right: '0', opacity: 1 }),
                 animate('.3s ease-in',
-                    style({ height: 0, opacity: 1 }))
+                    style({ right: '-100vw', opacity: 0 }))
             ]
         )
     ]
